217bfaf4f39c5137f13630968f91e206abac7200
dipu-bd <dipu.sudipta@gmail.com>
2018-02-21 21:48:19 +0600

    Reduce dependency

--- EshebaBot/helper.py
+++ EshebaBot/helper.py
@@ -1,21 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Helper methods used in crawling"""
-import json
-from os import path, makedirs
-from splinter import Browser
 from captcha_solver import CaptchaSolver
-
-CHROME_DRIVER = path.join('lib', 'chromedriver')
-
-
-def get_browser():
-    '''open a headless chrome browser in incognito mode'''
-    return Browser('chrome',
-                #    headless=True,
-                   incognito=True,
-                   executable_path=CHROME_DRIVER)
-# end def
 
 def solve_captcha(image):
     '''get captcha code from image url'''
