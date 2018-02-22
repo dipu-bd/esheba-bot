bb73922709d94c89715ab1d73e803a32ceef76f2
dipu-bd <dipu.sudipta@gmail.com>
2018-02-22 22:32:17 +0600

    Change esheba bot to use as service

--- EshebaBot/esheba.py
+++ EshebaBot/esheba.py
@@ -18,93 +18,105 @@ from .helper import solve_captcha
 class EshebaBot:
     '''Create REST bot to esheba'''
 
-    def __init__(self, useremail, password):
-        if (not useremail) or (not password):
-            raise Exception('User email and password is required')
-        # end if
-        self.useremail = useremail
-        self.password = password
-        self.captcha = '1fc9186ed74f4a6cb00b55e88de6b392'
-        self.security_code = '13193'
+    def __init__(self):
         urllib3.disable_warnings()
+        self.session = requests.Session()
+        self.session.headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36'
     # end def
 
-    def start(self):
-        '''start crawling'''
-        session = requests.Session()
-        session.headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36'
-        try:
-            self.get_tokens(session)
-            self.solve_captcha()
-            self.login(session)
-            self.get_personal_info(session)
-        finally:
-            session.close()
-        # end try
-    # end def
-
-    def get_tokens(self, session):
-        '''get php session id and captcha code'''
+    def start_session(self):
+        '''starts the session and get captcha'''
         url = 'https://www.esheba.cnsbd.com/index'
-        print('Getting Session ID:', url)
-        r = session.get(url, verify=False)
-        cookies = session.cookies.get_dict()
+        r = self.session.get(url, verify=False)
+        cookies = self.session.cookies.get_dict()
         self.token = cookies['PHPSESSID']
         soup = BeautifulSoup(r.content, 'lxml')
         captcha = soup.select_one('img#captcha')['src']
         self.captcha = path.basename(captcha).strip('.png')
-        print('Session ID =', self.token)
-        print('Captcha =', self.captcha)
     # end def
 
-    def solve_captcha(self):
+    def close_session(self):
+        '''stops the session'''
+        self.session.close()
+    # end def
+
+    def get_captcha_url(self):
         '''Generate text from captcha'''
-        url = 'https://www.esheba.cnsbd.com/images/captcha/' + self.captcha + '.png'
-        print('Getting image:', url)
-        r = requests.get(url, verify=False)
-        self.security_code = solve_captcha(r.content)
-        print('Solved captcha =', self.security_code)
+        if not self.captcha:
+            raise Exception('No captcha found!')
+        else:
+            return 'https://www.esheba.cnsbd.com/images/captcha/%s.png' % self.captcha
+        # end if
     #end def
 
-    def login(self, session):
+    def login(self, usermail, password, security_code):
         '''login to the website'''
         url = 'https://www.esheba.cnsbd.com/index'
         data = {
-            'usermail': self.useremail,
-            'password': self.password,
-            'security_code': self.security_code,
+            'usermail': usermail,
+            'password': password,
+            'security_code': security_code,
             'captcha[id]': self.captcha,
             'signin': 'SIGN-IN'
         }
         headers = { 'Content-Type': 'application/x-www-form-urlencoded' }
-        r = session.post(url, data=data, headers=headers)
+        r = self.session.post(url, data=data, headers=headers)
         soup = BeautifulSoup(r.content, 'lxml')
         errors = soup.select('#messages .error-msg')
         errors = [x.text.strip() for x in errors if x.text.strip()]
-        if len(errors):
-            print('Error: ', '; '.join(errors))
-            return False
-        # end if
+        return '\n'.join(errors).strip()
     # end def
 
-    def get_personal_info(self, session):
+    def check_session(self):
+        url = 'https://www.esheba.cnsbd.com/account/index'
+        r = self.session.get(url, verify=False)
+        soup = BeautifulSoup(r.content, 'lxml')
+        captcha = soup.select('img#captcha')['src']
+        return len(captcha) == 0
+    # end def
+
+    def get_personal_info(self):
         '''gets the personal informations'''
         url = 'https://www.esheba.cnsbd.com/account/index'
-        r = session.get(url, verify=False)
+        r = self.session.get(url, verify=False)
         soup = BeautifulSoup(r.content, 'lxml')
         table_headers = soup.select('#dashboard .table .home th')
         table_bodies = soup.select('#dashboard .table .home td')
-        print('\n--- Personal Informations ---')
-        for i in range(min(len(table_bodies), len(table_headers))):
-            print(' ', table_headers[i].text, '=', table_bodies[i].text)
+        data = dict()
+        for i, th in enumerate(table_headers):
+            header = th.text.strip()
+            if header == 'Name':
+                data['Name'] = table_bodies[i].text
+            elif header == 'Email Address':
+                data['EmailAddress'] = table_bodies[i].text
+            elif header == 'Address':
+                data['Address'] = table_bodies[i].text
+            elif header == 'Cell Phone Number':
+                data['PhoneNumber'] = table_bodies[i].text
+            # end if
         # end for
+        return data
     # end def
 
+    def test(self, usermail, password):
+        '''test if the service works'''
+        print('Starting session')
+        self.start_session()
+        print('Session ID =', self.token)
+        print('Captcha =', self.captcha)
+        captcha_url = self.get_captcha_url()
+        r = self.session.get(captcha_url, verify=False)
+        security_code = solve_captcha(r.content)
+        r = self.login(usermail, password, security_code)
+        print('Login response =', r)
+        r = self.get_personal_info()
+        print('Personal informations =', r)
+    # end def
 # end class
 
 if __name__ == '__main__':
-    EshebaBot(
-        useremail=sys.argv[1],
+    EshebaBot().test(
+        usermail=sys.argv[1],
         password=sys.argv[2]
-    ).start()
+    )
 # end if
