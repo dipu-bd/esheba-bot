b0f4a9475c5f4cfd5c7f70feb7e511942a0ff37b
dipu-bd <dipu.sudipta@gmail.com>
2018-02-22 21:50:33 +0600

    Add run_server to server.py

--- server.py
+++ server.py
@@ -1,6 +1,43 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Implements grpc modules"""
+import grpc
+import service.service_pb2 as BotPB
+from service import service_pb2_grpc
+from concurrent.futures import ThreadPoolExecutor
 
-from grpc.service_pb2_grpc import BotServiceStub
+class BotService(service_pb2_grpc.BotServiceServicer):
 
+  def StartSession(self, request, context):
+    pass
+  # end def
+
+  def Login(self, request, context):
+    pass
+  # end def
+
+  def CheckSession(self, request, context):
+    pass
+  # end def
+
+  def GetPersonalInfos(self, request, context):
+    pass
+  # end def
+# end class
+
+
+def run_server():
+  '''Runs the grpc server'''
+  port = '5000'
+  server = grpc.server(ThreadPoolExecutor(max_workers=10))
+  service_pb2_grpc.add_BotServiceServicer_to_server(BotService(), server)
+  server.add_insecure_port('[::]:' + port)
+  server.start()
+  print('Server running at http://localhost:%s' % port)
+  input('Press enter to stop the server...')
+  server.stop(True)
+# end def
+
+if __name__ == '__main__':
+  run_server()
+# end if
