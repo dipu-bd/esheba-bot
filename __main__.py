7895300c879e98b1bb3e7e139cf0a38c580d8992
dipu-bd <dipu.sudipta@gmail.com>
2018-02-22 21:50:23 +0600

    Add more scripts

--- __main__.py
+++ __main__.py
@@ -2,18 +2,29 @@
 # -*- coding: utf-8 -*-
 """Main point of execution"""
 import sys
+from server import run_server
 from EshebaBot.esheba import EshebaBot
 
 
 def main():
     '''main method to call'''
-    if len(sys.argv) < 3:
+    if len(sys.argv) < 2:
         return show_help()
     # end if
 
-    email = sys.argv[1]
-    password = sys.argv[2]
-    EshebaBot(email, password).start()
+    if sys.argv[1] == '--server':
+        run_server()
+    elif sys.argv[1] == '--test':
+        print('no yet implemented')
+        pass
+    elif len(sys.argv) < 3:
+        return show_help()
+    else:
+        email = sys.argv[1]
+        password = sys.argv[2]
+        EshebaBot(email, password).start()
+    # end if
+
 # end def
 
 
@@ -21,10 +32,14 @@ def show_help():
     '''displays help'''
     print('EshebaBot:')
     print('  python . <user-email> <password>')
+    print('  python . --server')
+    print('  python . --test')
     print()
     print('OPTIONS:')
-    print('user-email*  Email of the user to login.')
-    print('password*    Password to login')
+    print(' user-email* Email of the user to login.')
+    print(' password*   Password to login')
+    print(' --server    Starts the grpc server')
+    print(' --test      Runs grpc test client')
     print()
     print('HINTS:')
     print('- * marked params are required')
